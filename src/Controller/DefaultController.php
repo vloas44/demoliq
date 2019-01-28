17b824a58cee27e2a766f3fc39c7b5a64a183315
LOISEL Aurélien <aurelien.loisel2018@campus-eni.fr>
2019-01-28 14:29:21 +0100

    Base deployed

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('default/home.html.twig');
+    }
+
+    /**
+     * @Route ("/faq",name="faq")
+     */
+    public function faq()
+    {
+        return $this->render('default/faq.html.twig');
+    }
+
+    /**
+     * @Route ("/cgu",name="cgu")
+     */
+    public function cgu()
+    {
+        return $this->render('default/cgu.html.twig');
+    }
+
+    /**
+     * @Route ("/fonctionnement",name="fonctionnement")
+     */
+    public function fonctionnement()
+    {
+        return $this->render('default/fonctionnement.html.twig');
+    }
+}
